9d476c8f696718ca280fb34375da3ce42234905b
ceddlyburge <ceddlyburge@gmail.com>
2020-09-29 10:39:37 +0100

    Add 40-0 server wins test, and use paramterised test

--- tests/test_kata.py
+++ tests/test_kata.py
@@ -1,29 +1,37 @@
+import pytest
 from src.kata import GameScore, PlayerScore
 
+server_wins = [
+    (
+        PlayerScore.Zero,
+        PlayerScore.Zero, 
+        GameScore(
+            server_score=PlayerScore.Fifteen,
+            receiver_score=PlayerScore.Zero)
+    ),
+    (
+        PlayerScore.Fifteen,
+        PlayerScore.Zero, 
+        GameScore(
+            server_score=PlayerScore.Thirty,
+            receiver_score=PlayerScore.Zero)
+    ),
+    (
+        PlayerScore.Thirty,
+        PlayerScore.Zero, 
+        GameScore(
+            server_score=PlayerScore.Fourty,
+            receiver_score=PlayerScore.Zero)
+    )
+]
 
-def test_0_0_server_wins():
+
+@pytest.mark.parametrize("server_score,receiver_score,expected_score", server_wins)
+def test_server_wins(server_score, receiver_score, expected_score):
     current_score = GameScore(
-        server_score=PlayerScore.Zero,
-        receiver_score=PlayerScore.Zero)
+        server_score=server_score,
+        receiver_score=receiver_score)
     
     new_score = current_score.server_wins_point()
-
-    expected_score = GameScore(
-        server_score=PlayerScore.Fifteen,
-        receiver_score=PlayerScore.Zero)
-
-    assert new_score == expected_score
-
-
-def test_15_0_server_wins():
-    current_score = GameScore(
-        server_score=PlayerScore.Fifteen,
-        receiver_score=PlayerScore.Zero)
-
-    new_score = current_score.server_wins_point()
-
-    expected_score = GameScore(
-        server_score=PlayerScore.Thirty,
-        receiver_score=PlayerScore.Zero)
 
     assert new_score == expected_score
