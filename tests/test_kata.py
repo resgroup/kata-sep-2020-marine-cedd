71885a2ff689d4fa693591036d4c6413f30048aa
Marine Lannic <marinelannic@hotmail.com>
2020-09-29 10:29:09 +0100

    15 - 0 server wins  test

--- tests/test_kata.py
+++ tests/test_kata.py
@@ -13,3 +13,17 @@ def test_0_0_server_wins():
         receiver_score=PlayerScore.Zero)
 
     assert new_score == expected_score
+
+
+def test_15_0_server_wins():
+    current_score = GameScore(
+        server_score=PlayerScore.Fifteen,
+        receiver_score=PlayerScore.Zero)
+
+    new_score = current_score.server_wins_point()
+
+    expected_score = GameScore(
+        server_score=PlayerScore.Thirty,
+        receiver_score=PlayerScore.Zero)
+
+    assert new_score == expected_score
