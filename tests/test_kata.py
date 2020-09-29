49eee7ef11bc76335b1757aec4c4296219f42392
ceddlyburge <ceddlyburge@gmail.com>
2020-09-29 12:34:12 +0100

    Add tests for receiver wins

--- tests/test_kata.py
+++ tests/test_kata.py
@@ -49,8 +49,6 @@ server_wins = [
         GameScore(
             PlayerScore.Advantage,
             PlayerScore.Fourty),
-        #duece_game_Score(),
-        #GameScore(duece=True),
         GameScore(server_score=PlayerScore.Wins, receiver_score=PlayerScore.Fourty)
     )
 ]
@@ -59,5 +57,64 @@ server_wins = [
 @pytest.mark.parametrize("current_score, expected_score", server_wins)
 def test_server_wins(current_score, expected_score):
     new_score = current_score.server_wins_point()
+
+    assert new_score == expected_score
+
+
+receiver_wins = [
+    (
+        GameScore(
+            receiver_score=PlayerScore.Zero,
+            server_score=PlayerScore.Zero),
+        GameScore(
+            receiver_score=PlayerScore.Fifteen,
+            server_score=PlayerScore.Zero)
+    ),
+    (
+        GameScore(
+            receiver_score=PlayerScore.Fifteen,
+            server_score=PlayerScore.Zero),
+        GameScore(
+            receiver_score=PlayerScore.Thirty,
+            server_score=PlayerScore.Zero)
+    ),
+    (
+        GameScore(
+            receiver_score=PlayerScore.Thirty,
+            server_score=PlayerScore.Zero),
+        GameScore(
+            receiver_score=PlayerScore.Fourty,
+            server_score=PlayerScore.Zero)
+    ),
+    (
+        GameScore(
+            receiver_score=PlayerScore.Fourty,
+            server_score=PlayerScore.Zero),
+        GameScore(
+            receiver_score=PlayerScore.Wins,
+            server_score=PlayerScore.Zero)
+    ),
+    (
+        GameScore(
+            receiver_score=PlayerScore.Fourty,
+            server_score=PlayerScore.Fourty),
+        GameScore(
+            receiver_score=PlayerScore.Advantage,
+            server_score=PlayerScore.Fourty)
+    ),
+    (
+        GameScore(
+            receiver_score=PlayerScore.Advantage,
+            server_score=PlayerScore.Fourty),
+        GameScore(
+            receiver_score=PlayerScore.Wins,
+            server_score=PlayerScore.Fourty)
+    )
+]
+
+
+@pytest.mark.parametrize("current_score,expected_score", server_wins)
+def test_receiver_wins(current_score, expected_score):
+    new_score = current_score.receiver_wins_point()
 
     assert new_score == expected_score
