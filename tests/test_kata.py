e9eb6846cf7e6b4c455c8ddc4bc0396746024923
ceddlyburge <ceddlyburge@gmail.com>
2020-09-29 09:33:20 +0100

    Write first 0-0 test

--- tests/test_kata.py
+++ tests/test_kata.py
@@ -1,4 +1,16 @@
-import src.kata
+from src.kata import update_score, GameScore, PlayerScore, Player
 
-def test_a_function():
-    assert a_function() == 1
+def test_0_0_server_wins():
+    current_score = GameScore(
+        server_score=PlayerScore.Zero,
+        receiver_score=PlayerScore.Zero)
+    
+    new_score = current_score.update_score(
+        current_score, 
+        winner=Player.Server)
+
+    expected_score = GameScore(
+        server_score=PlayerScore.Fifteen,
+        receiver_score=PlayerScore.Zero)
+
+    assert new_score == expected_score
